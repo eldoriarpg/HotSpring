1bdab35c27acfc049413b48494beaae2ad29907d
Florian Fülling <mail@chojo.de>
2021-01-09 14:11:20 +0100

    add notifies to hot spring register

--- src/main/java/de/eldoria/hotsprings/HotSpringRegister.java
+++ src/main/java/de/eldoria/hotsprings/HotSpringRegister.java
@@ -1,7 +1,7 @@
 package de.eldoria.hotsprings;
 
-import de.eldoria.eldoutilities.localization.Replacement;
 import de.eldoria.eldoutilities.messages.MessageSender;
+import de.eldoria.eldoutilities.messages.MessageType;
 import de.eldoria.hotsprings.config.Configuration;
 import de.eldoria.hotsprings.config.HotSpring;
 import org.bukkit.entity.Player;
@@ -13,14 +13,12 @@ import java.util.Map;
 
 public class HotSpringRegister {
     private final Map<Player, HotSpring> hotSprings = new HashMap<>();
-    private final Configuration configuration;
     private final Plugin plugin;
-    private final MessageSender sender;
 
     public HotSpringRegister(Configuration configuration, Plugin plugin) {
-        this.configuration = configuration;
         this.plugin = plugin;
-        this.sender = MessageSender.getPluginMessageSender(HotSprings.class);
+        this.configuration = configuration;
+        this.sender = MessageSender.getPluginMessageSender(plugin);
     }
 
     public Map<Player, HotSpring> getActiveHotSprings() {
@@ -35,28 +33,16 @@ public class HotSpringRegister {
             plugin.getLogger().info("Hot Spring with name " + springName + " is not defined. Using default.");
             hotSprings.put(player, HotSpring.DEFAULT);
         }
-        sendMessage(player, "spring.enter");
+        sender.sendLocalized(configuration.getSettings().getMessageMode(), MessageType.NORMAL, player,
+                "spring.enter");
     }
 
     public void unregisterPlayer(Player player) {
         hotSprings.remove(player);
-        sendMessage(player, "spring.leave");
+        sender.sendLocalized(configuration.getSettings().getMessageMode(), MessageType.NORMAL, player,
+                "spring.leave");
     }
 
-    private void sendMessage(Player player, String message, Replacement... replacements) {
-        switch (configuration.getSettings().getMessageMode()) {
-            case TITLE:
-                sender.sendLocalizedTitle(player, "2", message, "", 20, 50, 10, replacements);
-                break;
-            case SUBTITLE:
-                sender.sendLocalizedTitle(player, "2", "", message, 20, 50, 10, replacements);
-                break;
-            case CHAT:
-                sender.sendLocalizedMessage(player, message, replacements);
-                break;
-            case ACTION_BAR:
-                sender.sendLocalizedActionBar(player, message, replacements);
-                break;
-        }
-    }
+    protected final Configuration configuration;
+    protected final MessageSender sender;
 }
