1313170a56fc4298b865e6561386337cc07909c1
Florian Fülling <mail@chojo.de>
2021-01-09 23:20:36 +0100

    fix wrong default value. null as default

--- src/main/java/de/eldoria/hotsprings/worldguard/HotSpringFlag.java
+++ src/main/java/de/eldoria/hotsprings/worldguard/HotSpringFlag.java
@@ -4,26 +4,37 @@ import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.FlagContext;
 import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
 import de.eldoria.eldoutilities.localization.ILocalizer;
+import de.eldoria.eldoutilities.localization.Replacement;
 import de.eldoria.hotsprings.HotSprings;
 import de.eldoria.hotsprings.config.Configuration;
+import de.eldoria.hotsprings.config.HotSpring;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Collectors;
 
 public class HotSpringFlag extends Flag<String> {
     private final Configuration configuration;
-    private final ILocalizer localizer;
+    private ILocalizer localizer = null;
 
     public HotSpringFlag(Configuration configuration) {
         super("hotspring");
         this.configuration = configuration;
-        localizer = ILocalizer.getPluginLocalizer(HotSprings.class);
     }
 
     @Override
     public String parseInput(FlagContext context) throws InvalidFlagFormat {
         if (!configuration.hasHotSpring(context.getUserInput())) {
-            throw new InvalidFlagFormat(localizer.getMessage("invalidSpring"));
+            String names = configuration.getHotSpringNames();
+            throw new InvalidFlagFormat(loc().getMessage("error.invalidSpring", Replacement.create("VALUES", names)));
         }
         return context.getUserInput();
+    }
+
+    private ILocalizer loc() {
+        if (localizer == null) {
+            localizer = ILocalizer.getPluginLocalizer(HotSprings.class);
+        }
+        return localizer;
     }
 
     @Override
@@ -43,6 +54,8 @@ public class HotSpringFlag extends Flag<String> {
 
     @Override
     public String getDefault() {
-        return "default";
+        return null;
     }
+
+
 }
